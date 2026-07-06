49729b524a1d63360b64222fbd166415d8197a6e
Thierry Gravelaine <gravelains@gmail.com>
2026-07-06 14:55:07 +0200

    feat/clean backend

--- maillot-api/src/app.controller.ts
+++ maillot-api/src/app.controller.ts
@@ -9,9 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  //hello
-  @Get('/hello')
-  getHelloWorld(): string {
-    return this.appService.getHelloWorld();
-  }
 }
